d91ade117eea2b4963ba301d8bd4b8124ac7965a
Mouni840 <125848775+Mouni840@users.noreply.github.com>
2023-07-02 19:57:05 +0530

    Week-1

---
+++ Week-1/battleship.py
@@ -0,0 +1,320 @@
+"""
+Battleship Project
+Name: G Tarun Kumar
+Roll No: 20211A0815
+"""
+
+import battleship_tests as test
+
+project = "Battleship" # don't edit this
+
+### SIMULATION FUNCTIONS ###
+
+from tkinter import *
+import random
+
+EMPTY_UNCLICKED = 1
+SHIP_UNCLICKED = 2
+EMPTY_CLICKED = 3
+SHIP_CLICKED = 4
+
+
+'''
+makeModel(data)
+Parameters: dict mapping strs to values
+Returns: None
+'''
+def makeModel(data):
+    data["number of rows"] = 10
+    data["number of cols"] = 10
+    data["board size"] = 500
+    data["number of ships"] = 5
+    data["cell size"] = data["board size"]/data["number of rows"]
+    data["computer"] = emptyGrid(data["number of rows"], data["number of cols"])
+    #data["user"] = emptyGrid(data["number of rows"], data["number of cols"])
+    data["user"] = test.testGrid()
+    addShips(data["computer"], data["number of ships"])
+    return 
+
+
+'''
+makeView(data, userCanvas, compCanvas)
+Parameters: dict mapping strs to values ; Tkinter canvas ; Tkinter canvas
+Returns: None
+'''
+def makeView(data, userCanvas, compCanvas):
+    drawGrid(data, compCanvas, data["computer"], True)
+    drawGrid(data, userCanvas, data["user"], True)
+    return
+
+
+'''
+keyPressed(data, events)
+Parameters: dict mapping strs to values ; key event object
+Returns: None
+'''
+def keyPressed(data, event):
+    pass
+
+
+'''
+mousePressed(data, event, board)
+Parameters: dict mapping strs to values ; mouse event object ; 2D list of ints
+Returns: None
+'''
+def mousePressed(data, event, board):
+    pass
+
+#### WEEK 1 ####
+
+'''
+emptyGrid(rows, cols)
+Parameters: int ; int
+Returns: 2D list of ints
+'''
+def emptyGrid(rows, cols):
+    grid = []
+    for row in range(rows):
+        grid.append([])
+        d = grid[row]
+        for col in range(cols):
+            d.append(EMPTY_UNCLICKED)
+    return grid
+
+
+'''
+createShip()
+Parameters: no parameters
+Returns: 2D list of ints
+'''
+def createShip():
+    row = random.randint(1,8)
+    col = random.randint(1,8)
+    align = random.randint(0,1)
+
+    if align == 0:
+        ship = [[row-1,col],[row,col],[row-1,col]]
+    else:
+        ship = [[row,col-1],[row,col],[row,col-1]]
+    return ship
+
+
+'''
+checkShip(grid, ship)
+Parameters: 2D list of ints ; 2D list of ints
+Returns: bool
+'''
+def checkShip(grid, ship):
+    for i in ship:
+        x = i[0]
+        y = i[1]
+        if grid[x][y] != EMPTY_UNCLICKED:
+            return False
+    return True
+
+
+'''
+addShips(grid, numShips)
+Parameters: 2D list of ints ; int
+Returns: 2D list of ints
+'''
+def addShips(grid, numShips):
+    j = 0
+    while j < numShips:
+        ship = createShip()
+        if checkShip(grid,ship):
+            for i in ship:
+                x = i[0]
+                y = i[1]
+                grid[x][y] = SHIP_UNCLICKED
+            j += 1
+    return grid
+
+
+'''
+drawGrid(data, canvas, grid, showShips)
+Parameters: dict mapping strs to values ; Tkinter canvas ; 2D list of ints ; bool
+Returns: None
+'''
+def drawGrid(data, canvas, grid, showShips):
+    i = data["cell size"]
+    for row in range(data["number of rows"]):
+        for col in range(data["number of cols"]):
+            if grid[row][col] == SHIP_UNCLICKED:
+                canvas.create_rectangle(col*i, row*i, data["cell size"] +col*i, data["cell size"] +row*i, fill="yellow")
+            else:
+                canvas.create_rectangle(col*i, row*i, data["cell size"] +col*i, data["cell size"] +row*i, fill="blue")
+
+    
+
+
+### WEEK 2 ###
+
+'''
+isVertical(ship)
+Parameters: 2D list of ints
+Returns: bool
+'''
+def isVertical(ship):
+    return
+
+
+'''
+isHorizontal(ship)
+Parameters: 2D list of ints
+Returns: bool
+'''
+def isHorizontal(ship):
+    return
+
+
+'''
+getClickedCell(data, event)
+Parameters: dict mapping strs to values ; mouse event object
+Returns: list of ints
+'''
+def getClickedCell(data, event):
+    return
+
+
+'''
+drawShip(data, canvas, ship)
+Parameters: dict mapping strs to values ; Tkinter canvas; 2D list of ints
+Returns: None
+'''
+def drawShip(data, canvas, ship):
+    return
+
+
+'''
+shipIsValid(grid, ship)
+Parameters: 2D list of ints ; 2D list of ints
+Returns: bool
+'''
+def shipIsValid(grid, ship):
+    return
+
+
+'''
+placeShip(data)
+Parameters: dict mapping strs to values
+Returns: None
+'''
+def placeShip(data):
+    return
+
+
+'''
+clickUserBoard(data, row, col)
+Parameters: dict mapping strs to values ; int ; int
+Returns: None
+'''
+def clickUserBoard(data, row, col):
+    return
+
+
+### WEEK 3 ###
+
+'''
+updateBoard(data, board, row, col, player)
+Parameters: dict mapping strs to values ; 2D list of ints ; int ; int ; str
+Returns: None
+'''
+def updateBoard(data, board, row, col, player):
+    return
+
+
+'''
+runGameTurn(data, row, col)
+Parameters: dict mapping strs to values ; int ; int
+Returns: None
+'''
+def runGameTurn(data, row, col):
+    return
+
+
+'''
+getComputerGuess(board)
+Parameters: 2D list of ints
+Returns: list of ints
+'''
+def getComputerGuess(board):
+    return
+
+
+'''
+isGameOver(board)
+Parameters: 2D list of ints
+Returns: bool
+'''
+def isGameOver(board):
+    return
+
+
+'''
+drawGameOver(data, canvas)
+Parameters: dict mapping strs to values ; Tkinter canvas
+Returns: None
+'''
+def drawGameOver(data, canvas):
+    return
+
+
+### SIMULATION FRAMEWORK ###
+
+from tkinter import *
+
+def updateView(data, userCanvas, compCanvas):
+    userCanvas.delete(ALL)
+    compCanvas.delete(ALL)
+    makeView(data, userCanvas, compCanvas)
+    userCanvas.update()
+    compCanvas.update()
+
+def keyEventHandler(data, userCanvas, compCanvas, event):
+    keyPressed(data, event)
+    updateView(data, userCanvas, compCanvas)
+
+def mouseEventHandler(data, userCanvas, compCanvas, event, board):
+    mousePressed(data, event, board)
+    updateView(data, userCanvas, compCanvas)
+
+def runSimulation(w, h):
+    data = { }
+    makeModel(data)
+
+    root = Tk()
+    root.resizable(width=False, height=False) # prevents resizing window
+
+    # We need two canvases - one for the user, one for the computer
+    Label(root, text = "USER BOARD - click cells to place ships on your board.").pack()
+    userCanvas = Canvas(root, width=w, height=h)
+    userCanvas.configure(bd=0, highlightthickness=0)
+    userCanvas.pack()
+
+    compWindow = Toplevel(root)
+    compWindow.resizable(width=False, height=False) # prevents resizing window
+    Label(compWindow, text = "COMPUTER BOARD - click to make guesses. The computer will guess on your board.").pack()
+    compCanvas = Canvas(compWindow, width=w, height=h)
+    compCanvas.configure(bd=0, highlightthickness=0)
+    compCanvas.pack()
+
+    makeView(data, userCanvas, compCanvas)
+
+    root.bind("<Key>", lambda event : keyEventHandler(data, userCanvas, compCanvas, event))
+    compWindow.bind("<Key>", lambda event : keyEventHandler(data, userCanvas, compCanvas, event))
+    userCanvas.bind("<Button-1>", lambda event : mouseEventHandler(data, userCanvas, compCanvas, event, "user"))
+    compCanvas.bind("<Button-1>", lambda event : mouseEventHandler(data, userCanvas, compCanvas, event, "comp"))
+
+    updateView(data, userCanvas, compCanvas)
+
+    root.mainloop()
+
+
+### RUN CODE ###
+
+# This code runs the test cases to check your work
+#if __name__ == "__main__":
+
+    ## Finally, run the simulation to test it manually ##
+    # runSimulation(500, 500)
